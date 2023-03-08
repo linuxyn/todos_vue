15ac559e699ad3ee0146b574719e7eff957aae75
yangning <yangning@liulv.net>
2023-03-08 16:03:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,13 +6,18 @@ import VueRouter from 'vue-router'
 // 引入路由器
 import router from './router'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Row } from 'element-ui';
+
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
+Vue.use(Button);
+Vue.use(Row);
 
 Vue.config.productionTip = false
 // 使用插件
 Vue.use(VueRouter)
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
